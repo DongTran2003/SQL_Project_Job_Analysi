5b113cbf4bed71b32961848fc16711e81722cd23
Justin Tran <phucdong103@gmail.com>
2024-06-03 04:57:05 +1000

    Update
    xoa 3 file ngu ngoc va cho 2 file hehee

---
+++ sql_project/1_top_paying_job.sql
@@ -0,0 +1,130 @@
+/* 
+Identify the top 10 highest-paying Data Analyst roles within USA both onsite and remote 
+*/
+
+SELECT
+    job_title,
+    job_location,
+    job_schedule_type,
+    job_work_from_home,
+    salary_year_avg,
+    job_posted_date,
+    cd.name AS company_name
+FROM job_postings_fact
+LEFT JOIN company_dim as cd 
+    ON cd.company_id = job_postings_fact.company_id
+WHERE job_title_short = 'Data Analyst' AND
+    job_country = 'United States' AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10
+
+/*
+The top 10 highest paid Data Analyst jobs in the States are:
+- Sr Data Analyst
+- HC Data Analyst
+- Head of Infrastructure Management & Data Analytics - Financial...
+- Director of Safety Data Analysis
+- Data Analyst
+- Head of Data Analytics
+- Director of Analytics
+- Research Scientist
+- Associate Director- Data Insights
+- Partner Technology Manager, Data Analytics and AI
+
+
+[
+  {
+    "job_title": "Sr Data Analyst",
+    "job_location": "Bethesda, MD",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "375000.0",
+    "job_posted_date": "2023-04-05 12:00:12",
+    "company_name": "Illuminate Mission Solutions"
+  },
+  {
+    "job_title": "HC Data Analyst , Senior",
+    "job_location": "Bethesda, MD",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "375000.0",
+    "job_posted_date": "2023-08-18 07:00:22",
+    "company_name": "Illuminate Mission Solutions"
+  },
+  {
+    "job_title": "Head of Infrastructure Management & Data Analytics - Financial...",
+    "job_location": "Jacksonville, FL",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "375000.0",
+    "job_posted_date": "2023-07-03 11:30:01",
+    "company_name": "Citigroup, Inc"
+  },
+  {
+    "job_title": "Director of Safety Data Analysis",
+    "job_location": "Austin, TX",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "375000.0",
+    "job_posted_date": "2023-04-21 08:01:55",
+    "company_name": "Torc Robotics"
+  },
+  {
+    "job_title": "Data Analyst",
+    "job_location": "San Francisco, CA",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "350000.0",
+    "job_posted_date": "2023-06-22 07:00:59",
+    "company_name": "Anthropic"
+  },
+  {
+    "job_title": "Head of Data Analytics",
+    "job_location": "Austin, TX",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "350000.0",
+    "job_posted_date": "2023-10-23 05:01:05",
+    "company_name": "Care.com"
+  },
+  {
+    "job_title": "Director of Analytics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": true,
+    "salary_year_avg": "336500.0",
+    "job_posted_date": "2023-08-23 12:04:42",
+    "company_name": "Meta"
+  },
+  {
+    "job_title": "Research Scientist",
+    "job_location": "San Francisco, CA",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "285000.0",
+    "job_posted_date": "2023-04-19 18:04:21",
+    "company_name": "OpenAI"
+  },
+  {
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": true,
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T"
+  },
+  {
+    "job_title": "Partner Technology Manager, Data Analytics and AI",
+    "job_location": "Austin, TX",
+    "job_schedule_type": "Full-time",
+    "job_work_from_home": false,
+    "salary_year_avg": "254000.0",
+    "job_posted_date": "2023-07-28 13:01:20",
+    "company_name": "Google"
+  }
+]
+*/
+
+
---
+++ sql_project/2_top_paying_skills_required.sql
@@ -0,0 +1,347 @@
+/*
+Identify the skills required for that top 10 Data Analyst jobs
+*/
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        salary_year_avg,
+        cd.name AS company_name
+    FROM job_postings_fact AS job_postings
+    LEFT JOIN company_dim as cd 
+        ON cd.company_id = job_postings.company_id
+
+    WHERE job_title_short = 'Data Analyst' AND
+        job_country = 'United States' AND
+        salary_year_avg IS NOT NULL
+
+    ORDER BY salary_year_avg DESC
+   LIMIT 10
+)
+
+SELECT 
+    top_paying_jobs.*,
+    sd.skills
+FROM skills_job_dim AS sjd
+JOIN skills_dim as sd
+    ON sd.skill_id = sjd.skill_id
+JOIN top_paying_jobs 
+    ON top_paying_jobs.job_id = sjd.job_id
+ORDER BY salary_year_avg DESC
+
+/*
+Most Common Skills:
+
+Python: 5 occurrences
+SQL: 4 occurrences
+R: 4 occurrences
+Excel: 4 occurrences
+Tableau: 4 occurrences
+
+
+[
+  {
+    "job_id": 1110602,
+    "job_title": "HC Data Analyst , Senior",
+    "salary_year_avg": "375000.0",
+    "company_name": "Illuminate Mission Solutions",
+    "skills": "python"
+  },
+  {
+    "job_id": 1110602,
+    "job_title": "HC Data Analyst , Senior",
+    "salary_year_avg": "375000.0",
+    "company_name": "Illuminate Mission Solutions",
+    "skills": "r"
+  },
+  {
+    "job_id": 1110602,
+    "job_title": "HC Data Analyst , Senior",
+    "salary_year_avg": "375000.0",
+    "company_name": "Illuminate Mission Solutions",
+    "skills": "vba"
+  },
+  {
+    "job_id": 1110602,
+    "job_title": "HC Data Analyst , Senior",
+    "salary_year_avg": "375000.0",
+    "company_name": "Illuminate Mission Solutions",
+    "skills": "excel"
+  },
+  {
+    "job_id": 1110602,
+    "job_title": "HC Data Analyst , Senior",
+    "salary_year_avg": "375000.0",
+    "company_name": "Illuminate Mission Solutions",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 641501,
+    "job_title": "Head of Infrastructure Management & Data Analytics - Financial...",
+    "salary_year_avg": "375000.0",
+    "company_name": "Citigroup, Inc",
+    "skills": "excel"
+  },
+  {
+    "job_id": 641501,
+    "job_title": "Head of Infrastructure Management & Data Analytics - Financial...",
+    "salary_year_avg": "375000.0",
+    "company_name": "Citigroup, Inc",
+    "skills": "word"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "sql"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "python"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "r"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "sas"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "matlab"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "spark"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "airflow"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "excel"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "power bi"
+  },
+  {
+    "job_id": 229253,
+    "job_title": "Director of Safety Data Analysis",
+    "salary_year_avg": "375000.0",
+    "company_name": "Torc Robotics",
+    "skills": "sas"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "sql"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "python"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "r"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "bigquery"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "snowflake"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "power bi"
+  },
+  {
+    "job_id": 101757,
+    "job_title": "Head of Data Analytics",
+    "salary_year_avg": "350000.0",
+    "company_name": "Care.com",
+    "skills": "looker"
+  },
+  {
+    "job_id": 1059665,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "350000.0",
+    "company_name": "Anthropic",
+    "skills": "sql"
+  },
+  {
+    "job_id": 1059665,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "350000.0",
+    "company_name": "Anthropic",
+    "skills": "python"
+  },
+  {
+    "job_id": 894135,
+    "job_title": "Research Scientist",
+    "salary_year_avg": "285000.0",
+    "company_name": "OpenAI",
+    "skills": "github"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "sql"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "python"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "r"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "azure"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "databricks"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "aws"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "pyspark"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "jupyter"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "excel"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "power bi"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "powerpoint"
+  },
+  {
+    "job_id": 285766,
+    "job_title": "Partner Technology Manager, Data Analytics and AI",
+    "salary_year_avg": "254000.0",
+    "company_name": "Google",
+    "skills": "gcp"
+  }
+]
+
+*/
